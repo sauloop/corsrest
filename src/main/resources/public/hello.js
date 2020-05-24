76f9e597a442a7edd717f2a1408674aacf24e031
Pablo Giraldo <pablogirald@yahoo.es>
2020-05-24 12:03:51 +0200

    Restore cors test

--- src/main/resources/public/hello.js
+++ src/main/resources/public/hello.js
@@ -1,5 +1,8 @@
-angular.module('demo', []).controller('Hello', function($scope, $http) {
-	$http.get('https://corsrest.herokuapp.com/test').then(function(response) {
-		$scope.greeting = response.data;
-	});
-});
+angular.module('demo', []).controller(
+		'Hello',
+		function($scope, $http) {
+			$http.get('https://corsrest.herokuapp.com/greeting').then(
+					function(response) {
+						$scope.greeting = response.data;
+					});
+		});
