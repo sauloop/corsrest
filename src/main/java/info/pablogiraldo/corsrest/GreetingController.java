2b27e0575454f31db5dddcd141f7c9a14e2cca0f
Pablo Giraldo <pablogirald@yahoo.es>
2020-05-24 11:35:36 +0200

    Add base files

---
+++ src/main/java/info/pablogiraldo/corsrest/GreetingController.java
@@ -0,0 +1,29 @@
+package info.pablogiraldo.corsrest;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@CrossOrigin(origins = "http://localhost:9000")
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(required = false, defaultValue = "World") String name) {
+		System.out.println("==== in greeting ====");
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+
+	@GetMapping("/test")
+	public Greeting test(@RequestParam(required = false, defaultValue = "Test") String name) {
+		System.out.println("==== in greeting ====");
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+
+}
---
+++ src/test/java/info/pablogiraldo/corsrest/CorsrestApplicationTests.java
@@ -0,0 +1,13 @@
+package info.pablogiraldo.corsrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorsrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
