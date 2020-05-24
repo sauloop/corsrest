ca68048a0f39be3e8ee2763ecffb0512b3790567
Pablo Giraldo <pablogirald@yahoo.es>
2020-05-24 11:56:00 +0200

    Change url config

--- src/main/java/info/pablogiraldo/corsrest/GreetingController.java
+++ src/main/java/info/pablogiraldo/corsrest/GreetingController.java
@@ -13,7 +13,7 @@ public class GreetingController {
 	private static final String template = "Hello %s";
 	private final AtomicLong counter = new AtomicLong();
 
-	@CrossOrigin(origins = "http://corsrest.herokuapp.com:9000")
+	@CrossOrigin(origins = "https://corsrest.herokuapp.com")
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(required = false, defaultValue = "World") String name) {
 		System.out.println("==== in greeting ====");
--- src/main/resources/public/hello.js
+++ src/main/resources/public/hello.js
@@ -1,7 +1,7 @@
 angular.module('demo', []).controller(
 		'Hello',
 		function($scope, $http) {
-			$http.get('http://corsrest.herokuapp.com:8080/greeting').then(
+			$http.get('https://corsrest.herokuapp.com/greeting').then(
 					function(response) {
 						$scope.greeting = response.data;
 					});
