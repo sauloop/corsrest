3b7dd7835e5e981ef7fae2f59fdcb8aac455a05f
Pablo Giraldo <pablogirald@yahoo.es>
2020-05-24 11:53:04 +0200

    Change url config

--- src/main/resources/public/hello.js
+++ src/main/resources/public/hello.js
@@ -1,7 +1,7 @@
 angular.module('demo', []).controller(
 		'Hello',
 		function($scope, $http) {
-			$http.get('https://corsrest.herokuapp.com:8080/greeting').then(
+			$http.get('http://corsrest.herokuapp.com:8080/greeting').then(
 					function(response) {
 						$scope.greeting = response.data;
 					});
